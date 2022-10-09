f9f33f7db2c77b7db04f8918771010a232c21f6a
YashKumarSoni <115381856+YashKumarSoni@users.noreply.github.com>
2022-10-09 07:37:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit;
 
 streamlit.title("My Parents New Healthy Dinner!");
 
-streamlit.title("New Line!");
+streamlit.title("Breakfast menu!");
+streamlit.title("---------------------");
+streamlit.title("Oats!");
+streamlit.title("Bread omlette");
+streamlit.title("Milk");
